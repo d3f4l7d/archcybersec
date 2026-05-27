0947df11d74720ded72fe73552ff27be2bd1c9c9
d3f4l7d <idaten.bekko@gmail.com>
2026-05-27 02:43:02 +0000

    Update goodbyeworld.sh

--- goodbyeworld.sh
+++ goodbyeworld.sh
@@ -163,6 +163,8 @@ then
     exit
     
     sudo pacman -S --noconfirm ruby ruby-rdoc
+    gpg --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+    
 
     git clone https://github.com/rapid7/metasploit-framework.git ~/.config/msf/
     cd ~/.config/msf/
