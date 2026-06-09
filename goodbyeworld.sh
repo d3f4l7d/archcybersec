6f0eb40e3980122dea136f6e6bb7a48802627246
d3f4l7d <idaten.bekko@gmail.com>
2026-06-09 08:47:53 +0000

    Update goodbyeworld.sh

--- goodbyeworld.sh
+++ goodbyeworld.sh
@@ -160,8 +160,8 @@ fi
 
 
 if [ -e ~/.config/metasploit-framework/msfconsole ]; then
-    read -q "RESPONSE?Did you replaced <ownername>, ownername, <dbname>, dbname, and <ownerpasswd> in postgres section beforehand? (y/n): "
-    if [ "$RESPONSE" == "y" || "$RESPONSE" == "yes" ]; then
+    echo "Did you replaced <ownername>, ownername, <dbname>, dbname, and <ownerpasswd> in postgres section beforehand? (y/N): "
+    if read -q; then
         echo
         echo " __  __ ____  _____ "
         echo "|  \/  / ___||  ___|"
