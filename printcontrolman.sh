91fd04c26b3cb65f8197a3c96cffa963be6b528e
d3f4l7d <253656084+d3f4l7d@users.noreply.github.com>
2026-08-31 02:41:49 +0000

    Create printcontrolman.sh

---
+++ printcontrolman.sh
@@ -0,0 +1,33 @@
+if pacman -Q zsh &> /dev/null; then
+    #!/bin/zsh
+    date
+    echo "zsh detected ($USER@$HOSTNAME)"
+else
+    echo "zsh is not installed."
+    echo "This script was tested under zsh."
+    echo "Install zsh via pacman like: sudo pacman -S zsh && sudo chsh -s /bin/zsh $USER"
+    echo "Or remove this if section via your texteditor like: vim printcontrolman.sh"
+    exit
+fi
+
+
+echo
+echo "    _        _                                    __            _ _   "
+echo "   / \   ___| |_ _   _ ___   _ __   ___  _ __    / _| __ _  ___(_) |_ "
+echo "  / _ \ / __| __| | | / __| | '_ \ / _ \| '_ \  | |_ / _` |/ __| | __|"
+echo " / ___ \ (__| |_| |_| \__ \ | | | | (_) | | | | |  _| (_| | (__| | |_ "
+echo "/_/   \_\___|\__|\__,_|___/ |_| |_|\___/|_| |_| |_|  \__,_|\___|_|\__|"
+echo " _ __ ___ _   _ _ __ ___    _ __ (_)___(_)  _ __ ___   ___ _ __  ___  "
+echo "| '__/ _ \ | | | '_ ` _ \  | '_ \| / __| | | '_ ` _ \ / _ \ '_ \/ __| "
+echo "| | |  __/ |_| | | | | | | | | | | \__ \ | | | | | | |  __/ | | \__ \ "
+echo "|_|  \___|\__,_|_| |_| |_| |_| |_|_|___/_| |_| |_| |_|\___|_| |_|___/ "
+echo " ___(_) |_   _ __ ___  __ _                                           "
+echo "/ __| | __| | '__/ _ \/ _` |                                          "
+echo "\__ \ | |_  | | |  __/ (_| |                                          "
+echo "|___/_|\__| |_|  \___|\__,_|                                          "
+echo
+
+
+sudo pacman -Syuu
+
+
